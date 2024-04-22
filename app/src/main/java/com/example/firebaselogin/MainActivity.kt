991a1c375633c6010d2d8f53235b83a3a5e5005f
LenixRiz <rizky.fajarbusiness945@gmail.com>
2024-04-22 18:05:35 +0700

    Last Commit

--- app/src/main/java/com/example/firebaselogin/MainActivity.kt
+++ app/src/main/java/com/example/firebaselogin/MainActivity.kt
@@ -7,8 +7,12 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
 import android.widget.Toast
-import com.example.firebaselogin.databinding.ActivityLoginBinding
+import com.example.firebaselogin.LoginActivity
+import com.example.firebaselogin.MyListData
+import com.example.firebaselogin.R
+import com.example.firebaselogin.data_teman
 import com.example.firebaselogin.databinding.ActivityMainBinding
+import com.example.firebaselogin.databinding.ActivityLoginBinding
 import com.firebase.ui.auth.AuthUI
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
@@ -40,30 +44,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(p0: View?) {
         when (p0?.getId()) {
             R.id.save -> {
+                //Mendapatkan userID dari pengguna yang terautententikasi
                 val getUserID = auth!!.currentUser!!.uid
 
+                //mendapatkan Instance dari data base
                 val database = FirebaseDatabase.getInstance()
 
+                //Menyimpan data yang diimputkan user dalam variabel
                 val getNama: String = binding.nama.getText().toString()
                 val getAlamat: String = binding.alamat.getText().toString()
-                val getNoHp: String = binding.noHp.getText().toString()
+                val getNoHP: String = binding.noHp.getText().toString()
 
+                //Mendapatkan Refrensi dari database
                 val getReference: DatabaseReference
                 getReference = database.reference
 
-                if (isEmpty(getNama) || isEmpty(getAlamat) || isEmpty(getNoHp)) {
+                //mengecek apakah ada data yg kosong
+                if (isEmpty(getNama) || isEmpty(getAlamat) || isEmpty(getNoHP)) {
                     Toast.makeText(this@MainActivity, "Data tidak boleh ada yang kosong",
                         Toast.LENGTH_SHORT).show()
                 } else {
+                    //Jika teidak ada maka menyimpan ke database sesuai ID masing masing akun
                     getReference.child("Admin").child(getUserID).child("DataTeman").push()
-                        .setValue(data_teman(getNama, getAlamat, getNoHp))
-                        .addOnCompleteListener(this) {
+                        .setValue(data_teman(getNama, getAlamat, getNoHP))
+                        .addOnCompleteListener(this){
+                            //bagian yg terjadi ketika user berhasil menyimpan data
                             binding.nama.setText("")
                             binding.alamat.setText("")
                             binding.noHp.setText("")
-                            Toast.makeText(this@MainActivity, "Data tersimpan", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this@MainActivity, "Data Tersimpan",
+                                Toast.LENGTH_SHORT).show()
                         }
+
+
                 }
+
+
             }
             R.id.logout -> {
                 AuthUI.getInstance().signOut(this)
@@ -76,7 +92,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                         }
                     })
             }
-            R.id.show_data -> {}
+            R.id.show_data -> {
+                startActivity(Intent(this@MainActivity, MyListData::class.java))
+            }
         }
     }
 }
